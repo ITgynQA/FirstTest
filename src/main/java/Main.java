0e6bbdef16985f98985a1bf7be4e8aa2cc3d716b
Ekaterina Evdokimova <kate_evd79@mail.ru>
2023-03-05 14:58:51 +0400

    Modified Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
 
-        service.calculate(5000,true);
+        service.calculate(5000, true);
 
     }
 }
